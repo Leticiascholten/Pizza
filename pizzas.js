e34f56c8765aa38bcdc36268a42faa3089786d05
Letícia Scholten <129871586+Leticiascholten@users.noreply.github.com>
2023-04-11 14:59:18 -0300

    Add files via upload

---
+++ pizzas.js
@@ -0,0 +1,9 @@
+let pizzaJson = [
+    {id:1, name:'Mussarela', img:'images/pizza.png', price:20.19, sizes:['100g', '530g', '860g'], description:'A Pizza de Mussarela é coberta com molho de tomate, queijo tipo mussarela, azeitonas pretas e orégano e massa com fermentação natural.'},
+    {id:2, name:'Calabresa', img:'images/pizza2.png', price:18.00, sizes:['320g', '530g', '860g'], description:'A Pizza de Calabresa é coberta com molho de tomate, mussarela, calabresa fatiada e bacon.'},
+    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:17.45, sizes:['320g', '530g', '860g'], description:'A pizza de Pizza 4 Queijos é coberta com molho de tomate, muçarela, provolone, gorgonzola e parmesão ralado'},
+    {id:4, name:'Americana', img:'images/pizza4.png', price:19.77, sizes:['320g', '530g', '860g'], description:'A pizza Americana é coberta com molho de tomate, presunto e mussarela.'},
+    {id:5, name:'Sorvete', img:'images/pizza5.png', price:21.43, sizes:['320g', '530g', '860g'], description:'A pizza de sorvete é coberta com chocolate ralado, sorvete de creme e morango fatiado.'},
+    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:18.55, sizes:['320g', '530g', '860g'], description:'A pizza Moda da Casa é coberta com molho de tomate, uma saborosa combinação do bacon, calabresa e do presunto.'},
+    {id:7, name:'Chocolate', img:'images/pizza7.png', price:22.36, sizes:['320g', '530g', '860g'], description:'A pizza de chocolate é recheada com choolate ralado e confeti.'}
+];
